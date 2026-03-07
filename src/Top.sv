6bac7e6077048de2b71797b0720505f989a0b4b7
Jun <b12901143@ntu.edu.tw>
2026-03-07 21:00:27 +0800

    use time as seed

--- src/Top.sv
+++ src/Top.sv
@@ -13,20 +13,26 @@ parameter S_PROC = 1'b1;
 logic state_r, state_w;
 logic clk_en;
 logic lfsr_en;
+logic [15:0] time_seed;
+logic lfsr_load;
+
+assign lfsr_load = (state_r == S_IDLE) && i_start;
 assign lfsr_en = (state_r == S_PROC) && clk_en;
 
 // ===== Sub-Module Instantiation =====
 clk_counter u_clk_counter (
 	.i_clk   (i_clk),
 	.i_rst_n (i_rst_n),
-	.o_clk_en(clk_en)
+	.o_clk_en(clk_en),
+	.o_time_seed (time_seed)
 );
 
 lfsr_random_gen u_lfsr_random_gen (
 	.i_clk   (i_clk),
 	.i_rst_n (i_rst_n),
 	.i_en    (lfsr_en),
-	.i_seed  (16'd1),
+	.i_load  (lfsr_load),
+    .i_seed  (time_seed),
 	.o_rand  (o_random_out)
 );
 
@@ -66,9 +72,11 @@ endmodule
 module clk_counter(
 	input logic i_clk,
 	input logic i_rst_n,
-	output logic o_clk_en
+	output logic o_clk_en,
+	output logic [15:0] o_time_seed
 );
-	reg[17:0] counter;
+	reg [17:0] counter;
+	assign o_time_seed = counter[15:0];
 	
 	always_ff @(posedge i_clk or negedge i_rst_n) begin
 		// reset
@@ -93,6 +101,7 @@ module lfsr_random_gen (
     input  logic        i_clk,
     input  logic        i_rst_n,
     input  logic        i_en,
+    input  logic        i_load,
     input  logic [15:0] i_seed,
     output logic [3:0]  o_rand
 );
@@ -106,8 +115,12 @@ module lfsr_random_gen (
     always_comb begin
         lfsr_w = lfsr_r;
         
-        if (i_en) begin
-			// Taps: 16, 14, 13, 11 (x^16 + x^14 + x^13 + x^11 + 1)
+        if (i_load) begin
+			// If seed is zero, use a default non-zero value to avoid getting stuck in the zero state
+            lfsr_w = (i_seed == 16'd0) ? 16'hACE0 : i_seed;
+        end
+        else if (i_en) begin
+            // Taps: 16, 14, 13, 11 (x^16 + x^14 + x^13 + x^11 + 1)
             lfsr_w = {lfsr_r[14:0], lfsr_r[15] ^ lfsr_r[13] ^ lfsr_r[12] ^ lfsr_r[10]};
         end
     end
@@ -115,12 +128,7 @@ module lfsr_random_gen (
     // ===== Sequential Circuits =====
     always_ff @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
-			// If seed is zero, initialize to a non-zero value (e.g., 0xACE1)
-            if (i_seed == 16'd0) begin
-                lfsr_r <= 16'hACE1;
-            end else begin
-                lfsr_r <= i_seed;
-            end
+            lfsr_r <= 16'hACE0;
         end
         else begin
             lfsr_r <= lfsr_w;
